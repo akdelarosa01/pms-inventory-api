d011b30d89ddd5f05488ec432398d02e75073bc1
Kurt Dela Rosa <ak.delarosa01@gmail.com>
2023-09-11 17:39:09 +0000

    fixing CORS Policy

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,7 +45,7 @@ class Kernel extends HttpKernel
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Fruitcake\Cors\HandleCors::class,
+            // \Fruitcake\Cors\HandleCors::class,
         ],
     ];
 
